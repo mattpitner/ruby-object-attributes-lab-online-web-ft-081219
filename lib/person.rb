ad611251a0344f9c45af682c8187b0471e6564e5
Matt Pitner <mattpitner09@gmail.com>
2019-08-21 15:10:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,21 @@
 class Person
   
-  name=(persons_name)
+ def name=(persons_name)
   @name = persons_name
 end 
+
+def name 
+  @name 
+end 
+
+def job=(persons_job)
+  @job = persons_job
+end 
+
+def job 
+  @job
+end 
+
+
   
 end 
